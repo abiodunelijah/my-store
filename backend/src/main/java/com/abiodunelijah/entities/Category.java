f06c1c3b3fa6e7879901a37efba85fa51f79fb0b
sir-abbey <kaybiel4u@gmail.com>
2026-03-09 14:04:11 +0100

    Working on the backend. Implementing the category entity.

--- backend/src/main/java/com/abiodunelijah/entities/Category.java
+++ backend/src/main/java/com/abiodunelijah/entities/Category.java
@@ -1,4 +1,24 @@
 package com.abiodunelijah.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category ")
+    private List<Product> products;
 }

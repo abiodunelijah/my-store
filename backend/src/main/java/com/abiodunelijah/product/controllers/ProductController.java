cb934c285ce0af297977306817ca71cfeb2b546d
sir-abbey <kaybiel4u@gmail.com>
2026-03-16 11:50:25 +0100

    Working on the backend. Implementing Category Controller.

--- backend/src/main/java/com/abiodunelijah/product/controllers/ProductController.java
+++ backend/src/main/java/com/abiodunelijah/product/controllers/ProductController.java
@@ -1,4 +1,14 @@
 package com.abiodunelijah.product.controllers;
 
+import com.abiodunelijah.product.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
 }

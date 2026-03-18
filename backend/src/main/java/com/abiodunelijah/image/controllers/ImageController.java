ebbe1a48c629f3ab134622e89cc6c9559e3ffadd
sir-abbey <kaybiel4u@gmail.com>
2026-03-18 19:50:48 +0100

    Working on the backend. Implementing Image Controller.

--- backend/src/main/java/com/abiodunelijah/image/controllers/ImageController.java
+++ backend/src/main/java/com/abiodunelijah/image/controllers/ImageController.java
@@ -1,4 +1,30 @@
 package com.abiodunelijah.image.controllers;
 
+import com.abiodunelijah.image.dtos.ImageDto;
+import com.abiodunelijah.image.services.ImageService;
+import com.abiodunelijah.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/images")
 public class ImageController {
+
+
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> uploadImages(@RequestParam("files") List<MultipartFile> files,
+                                                    @RequestParam("productId") Long productId) {
+        List<ImageDto> imageDto = imageService.saveImages(productId, files);
+        return ResponseEntity.ok(new ApiResponse("Images uploaded successfully!", imageDto));
+    }
 }

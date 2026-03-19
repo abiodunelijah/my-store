6cc3578ebf7ac8f70ec0fcc5723a00af7c012fcd
sir-abbey <kaybiel4u@gmail.com>
2026-03-19 08:30:24 +0100

    Working on the backend. Implementing Image Controller.

--- backend/src/main/java/com/abiodunelijah/image/controllers/ImageController.java
+++ backend/src/main/java/com/abiodunelijah/image/controllers/ImageController.java
@@ -1,16 +1,19 @@
 package com.abiodunelijah.image.controllers;
 
 import com.abiodunelijah.image.dtos.ImageDto;
+import com.abiodunelijah.image.entities.Image;
 import com.abiodunelijah.image.services.ImageService;
 import com.abiodunelijah.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -26,5 +29,31 @@ public class ImageController {
                                                     @RequestParam("productId") Long productId) {
         List<ImageDto> imageDto = imageService.saveImages(productId, files);
         return ResponseEntity.ok(new ApiResponse("Images uploaded successfully!", imageDto));
+    }
+
+    @GetMapping("/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+
+        Image image = imageService.getImageById(imageId);
+
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment: filename=\"" + image.getFileName() + "\"")
+                .body(resource);
+
+    }
+
+    @PutMapping("/update/{imageId}")
+    public ResponseEntity<ApiResponse> updateImage(@RequestBody MultipartFile file, @PathVariable Long imageId) {
+
+        imageService.updateImage(file, imageId);
+        return ResponseEntity.ok(new ApiResponse("image updated successfully", null));
+    }
+
+    @DeleteMapping("/delete/{imageId}")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        imageService.deleteImageById(imageId);
+        return ResponseEntity.ok(new ApiResponse("image deleted successfully", null));
     }
 }

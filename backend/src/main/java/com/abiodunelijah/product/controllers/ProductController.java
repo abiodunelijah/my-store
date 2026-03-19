9e687f3108c0f654daca441eb3bd746826aa5f58
sir-abbey <kaybiel4u@gmail.com>
2026-03-19 08:53:21 +0100

    Working on the backend. Implementing Product Controller.

--- backend/src/main/java/com/abiodunelijah/product/controllers/ProductController.java
+++ backend/src/main/java/com/abiodunelijah/product/controllers/ProductController.java
@@ -1,14 +1,26 @@
 package com.abiodunelijah.product.controllers;
 
+import com.abiodunelijah.product.entities.Product;
 import com.abiodunelijah.product.services.ProductService;
+import com.abiodunelijah.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
+
     private final ProductService productService;
+
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("Products", products));
+    }
+    
 
 }
